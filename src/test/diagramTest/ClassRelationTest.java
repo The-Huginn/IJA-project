5d97d66ab81dea50792e014f24ca0ba79caa562a
Rastislav Budinsky <rastislavbudinsky@gmail.com>
2022-03-06 13:14:25 +0100

    Update ClassRelation Test

--- src/test/diagramTest/ClassRelationTest.java
+++ src/test/diagramTest/ClassRelationTest.java
@@ -3,6 +3,8 @@ package test.diagramTest;
 import backend.diagramObject.UMLClass;
 import backend.diagram.ClassRelation;
 
+import test.diagramObjectTest.helpers.TypeHelper;
+
 import java.util.ArrayList;
 import javafx.util.Pair;
 
@@ -16,6 +18,7 @@ public class ClassRelationTest {
 
     @BeforeEach
     public void setup() {
+        TypeHelper.setup();
         classes = new ArrayList<>();
         for (int i = 0; i < 4; i++)
             classes.add(new UMLClass("first class" + String.valueOf(i)));
@@ -23,24 +26,29 @@ public class ClassRelationTest {
 
     @AfterEach
     public void tearDown() {
+        TypeHelper.tearDown();
         classes.clear();
+    }
+
+    private Pair<UMLClass, Integer> createPair(UMLClass class1, int number) {
+        return new Pair<UMLClass, Integer>(class1, number);
     }
 
     @Test
     public void getterTest() {
-        ClassRelation relation = new ClassRelation("relation", new Pair<UMLClass, UMLClass>(classes.get(0), classes.get(1)));
+        ClassRelation relation = new ClassRelation("relation", new Pair<Pair<UMLClass, Integer>, Pair<UMLClass, Integer>>(createPair(classes.get(0), 0), createPair(classes.get(1), 0)));
         Assert.assertTrue(relation.getType() == ClassRelation.ClassRelEnum.ASSOCIATION);
-        Assert.assertTrue(relation.getFirst() == classes.get(0));
-        Assert.assertTrue(relation.getSecond() == classes.get(1));
+        Assert.assertTrue(relation.getFirst().getKey() == classes.get(0));
+        Assert.assertTrue(relation.getSecond().getKey() == classes.get(1));
     }
 
     @Test
     public void setRelationTest() {
         ClassRelation relation = new ClassRelation("relation");
-        relation.setFirst(classes.get(0));
-        relation.setSecond(classes.get(1));
-        Assert.assertTrue(relation.getFirst() == classes.get(0));
-        Assert.assertTrue(relation.getSecond() == classes.get(1));
+        relation.setFirst(createPair(classes.get(0), 0));
+        relation.setSecond(createPair(classes.get(1), 0));
+        Assert.assertTrue(relation.getFirst().getKey() == classes.get(0));
+        Assert.assertTrue(relation.getSecond().getKey() == classes.get(1));
     }
 
     @Test
@@ -52,19 +60,19 @@ public class ClassRelationTest {
 
     @Test
     public void changePeersTest() {
-        ClassRelation relation = new ClassRelation("relation", new Pair<UMLClass, UMLClass>(classes.get(0), classes.get(1)));
-        relation.setFirst(classes.get(2));
-        Assert.assertTrue(relation.getFirst() == classes.get(2));
-        relation.setSecond(classes.get(3));
-        Assert.assertTrue(relation.getSecond() == classes.get(3));
+        ClassRelation relation = new ClassRelation("relation", new Pair<Pair<UMLClass, Integer>, Pair<UMLClass, Integer>>(createPair(classes.get(0), 0), createPair(classes.get(1), 0)));
+        relation.setFirst(createPair(classes.get(2), 0));
+        Assert.assertTrue(relation.getFirst().getKey() == classes.get(2));
+        relation.setSecond(createPair(classes.get(3), 0));
+        Assert.assertTrue(relation.getSecond().getKey() == classes.get(3));
     }
 
     @Test
     public void samePeerTest() {
-        ClassRelation relation = new ClassRelation("relation", new Pair<UMLClass, UMLClass>(classes.get(0), classes.get(1)));
-        relation.setSecond(classes.get(0));
+        ClassRelation relation = new ClassRelation("relation", new Pair<Pair<UMLClass, Integer>, Pair<UMLClass, Integer>>(createPair(classes.get(0), 0), createPair(classes.get(1), 0)));
+        relation.setSecond(createPair(classes.get(0), 0));
         relation.setType(ClassRelation.ClassRelEnum.COMPOSITION);
-        Assert.assertTrue(relation.getFirst() == relation.getSecond());
+        Assert.assertTrue(relation.getFirst().getKey() == relation.getSecond());
         Assert.assertTrue(relation.getType() == ClassRelation.ClassRelEnum.COMPOSITION);
     }
 }
