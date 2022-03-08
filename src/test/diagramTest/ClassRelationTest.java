b4964c63f84a2c08be6c4aaa6e3eda794b82d6ac
Rastislav Budinsky <rastislavbudinsky@gmail.com>
2022-03-08 20:26:51 +0100

    Update tests for relations

--- src/test/diagramTest/ClassRelationTest.java
+++ src/test/diagramTest/ClassRelationTest.java
@@ -21,7 +21,7 @@ public class ClassRelationTest {
         TypeHelper.setup();
         classes = new ArrayList<>();
         for (int i = 0; i < 4; i++)
-            classes.add(new UMLClass("first class" + String.valueOf(i)));
+            classes.add(new UMLClass("first class" + String.valueOf(i), null));
     }
 
     @AfterEach
@@ -30,13 +30,13 @@ public class ClassRelationTest {
         classes.clear();
     }
 
-    private Pair<UMLClass, Integer> createPair(UMLClass class1, int number) {
-        return new Pair<UMLClass, Integer>(class1, number);
-    }
+    // private Pair<UMLClass, Integer> createPair(UMLClass class1, int number) {
+    //     return new Pair<UMLClass, Integer>(class1, number);
+    // }
 
     @Test
     public void getterTest() {
-        ClassRelation relation = new ClassRelation("relation", new Pair<Pair<UMLClass, Integer>, Pair<UMLClass, Integer>>(createPair(classes.get(0), 0), createPair(classes.get(1), 0)));
+        ClassRelation relation = new ClassRelation("relation", null, classes.get(0), 0, classes.get(1), 0);
         Assert.assertTrue(relation.getType() == ClassRelation.ClassRelEnum.ASSOCIATION);
         Assert.assertTrue(relation.getFirst().getKey() == classes.get(0));
         Assert.assertTrue(relation.getSecond().getKey() == classes.get(1));
@@ -44,35 +44,35 @@ public class ClassRelationTest {
 
     @Test
     public void setRelationTest() {
-        ClassRelation relation = new ClassRelation("relation");
-        relation.setFirst(createPair(classes.get(0), 0));
-        relation.setSecond(createPair(classes.get(1), 0));
+        ClassRelation relation = new ClassRelation("relation", null);
+        relation.setFirst(classes.get(0), 0);
+        relation.setSecond(classes.get(1), 0);
         Assert.assertTrue(relation.getFirst().getKey() == classes.get(0));
         Assert.assertTrue(relation.getSecond().getKey() == classes.get(1));
     }
 
     @Test
     public void changeTypeTest() {
-        ClassRelation relation = new ClassRelation("relation");
+        ClassRelation relation = new ClassRelation("relation", null);
         relation.setType(ClassRelation.ClassRelEnum.AGGREGATION);
         Assert.assertTrue(relation.getType() == ClassRelation.ClassRelEnum.AGGREGATION);
     }
 
     @Test
     public void changePeersTest() {
-        ClassRelation relation = new ClassRelation("relation", new Pair<Pair<UMLClass, Integer>, Pair<UMLClass, Integer>>(createPair(classes.get(0), 0), createPair(classes.get(1), 0)));
-        relation.setFirst(createPair(classes.get(2), 0));
+        ClassRelation relation = new ClassRelation("relation", null, classes.get(0), 0, classes.get(1), 0);
+        relation.setFirst(classes.get(2), 0);
         Assert.assertTrue(relation.getFirst().getKey() == classes.get(2));
-        relation.setSecond(createPair(classes.get(3), 0));
+        relation.setSecond(classes.get(3), 0);
         Assert.assertTrue(relation.getSecond().getKey() == classes.get(3));
     }
 
     @Test
     public void samePeerTest() {
-        ClassRelation relation = new ClassRelation("relation", new Pair<Pair<UMLClass, Integer>, Pair<UMLClass, Integer>>(createPair(classes.get(0), 0), createPair(classes.get(1), 0)));
-        relation.setSecond(createPair(classes.get(0), 0));
+        ClassRelation relation = new ClassRelation("relation", null, classes.get(0), 0, classes.get(1), 0);
+        relation.setSecond(classes.get(0), 0);
         relation.setType(ClassRelation.ClassRelEnum.COMPOSITION);
-        Assert.assertTrue(relation.getFirst().getKey() == relation.getSecond());
+        Assert.assertTrue(relation.getFirst().getKey() == relation.getSecond().getKey());
         Assert.assertTrue(relation.getType() == ClassRelation.ClassRelEnum.COMPOSITION);
     }
 }
--- src/test/diagramTest/SeqRelationTest.java
+++ src/test/diagramTest/SeqRelationTest.java
@@ -25,9 +25,11 @@ public class SeqRelationTest {
         TypeHelper.setup();
         classes = new ArrayList<>();
         for (int i = 0; i < 4; i++)
-            classes.add(new UMLClass("first class" + String.valueOf(i)));
-        method = new Method("method", Type.getType("int"), new String[] {"int", "string"});
-        attribute = new Attribute("attribute", Type.getType("int"));
+            classes.add(new UMLClass("first class" + String.valueOf(i), null));
+        method = new Method("method", classes.get(0), Type.getType("int"), Attribute.Visibility.PUBLIC, new String[] {"int", "string"});
+        classes.get(0).addMethod(method);
+        attribute = new Attribute("attribute", classes.get(0) ,Type.getType("int"), Attribute.Visibility.PUBLIC);
+        classes.get(0).addVariable(attribute);
     }
 
     @AfterEach
@@ -38,13 +40,13 @@ public class SeqRelationTest {
         attribute = null;
     }
 
-    private Pair<UMLClass, Integer> createPair(UMLClass class1, int number) {
-        return new Pair<UMLClass, Integer>(class1, number);
-    }
+    // private Pair<UMLClass, Integer> createPair(UMLClass class1, int number) {
+    //     return new Pair<UMLClass, Integer>(class1, number);
+    // }
 
     @Test
     public void getterTest() {
-        SeqRelation relation = new SeqRelation("relation", new Pair<Pair<UMLClass, Integer>, Pair<UMLClass, Integer>>(createPair(classes.get(0), 0), createPair(classes.get(1), 0)));
+        SeqRelation relation = new SeqRelation("relation", null, classes.get(0), 0, classes.get(1), 0);
         Pair<UMLClass, Integer> pair = relation.getFirst();
         Assert.assertTrue(relation.getType() == SeqRelation.SeqRelEnum.SYNCHROUNOUS);
         Assert.assertTrue(pair.getKey() == classes.get(0) && pair.getValue() == 0);
@@ -54,33 +56,33 @@ public class SeqRelationTest {
 
     @Test
     public void setRelationTest() {
-        SeqRelation relation = new SeqRelation("relation");
-        relation.setFirst(createPair(classes.get(0), 1));
-        relation.setSecond(createPair(classes.get(1), 2));
+        SeqRelation relation = new SeqRelation("relation", null);
+        relation.setFirst(classes.get(0), 1);
+        relation.setSecond(classes.get(1), 2);
         Assert.assertTrue(relation.getFirst().getKey() == classes.get(0) && relation.getFirst().getValue() == 1);
         Assert.assertTrue(relation.getSecond().getKey() == classes.get(1) && relation.getSecond().getValue() == 2);
     }
 
     @Test
     public void changeTypeTest() {
-        SeqRelation relation = new SeqRelation("relation");
+        SeqRelation relation = new SeqRelation("relation", null);
         relation.setType(SeqRelation.SeqRelEnum.RETURN);
         Assert.assertTrue(relation.getType() == SeqRelation.SeqRelEnum.RETURN);
     }
 
     @Test
     public void changePeersTest() {
-        SeqRelation relation = new SeqRelation("relation", new Pair<Pair<UMLClass, Integer>, Pair<UMLClass, Integer>>(createPair(classes.get(0), 0), createPair(classes.get(1), 0)));
-        relation.setFirst(createPair(classes.get(2), 2));
+        SeqRelation relation = new SeqRelation("relation", null, classes.get(0), 0, classes.get(1), 0);
+        relation.setFirst(classes.get(2), 2);
         Assert.assertTrue(relation.getFirst().getKey() == classes.get(2) && relation.getFirst().getValue() == 2);
-        relation.setSecond(createPair(classes.get(3), 3));
+        relation.setSecond(classes.get(3), 3);
         Assert.assertTrue(relation.getSecond().getKey() == classes.get(3) && relation.getSecond().getValue() == 3);
     }
 
     @Test
     public void samePeerTest() {
-        SeqRelation relation = new SeqRelation("relation", new Pair<Pair<UMLClass, Integer>, Pair<UMLClass, Integer>>(createPair(classes.get(0), 0), createPair(classes.get(1), 0)));
-        relation.setSecond(createPair(classes.get(0) 1));
+        SeqRelation relation = new SeqRelation("relation", null, classes.get(0), 0, classes.get(1), 0);
+        relation.setSecond(classes.get(0), 1);
         relation.setType(SeqRelation.SeqRelEnum.DESTRUCTION);
         Assert.assertTrue(relation.getFirst().getKey() == relation.getSecond().getKey());
         Assert.assertTrue(relation.getFirst().getValue() == 0 && relation.getSecond().getValue() == 1);
@@ -88,70 +90,67 @@ public class SeqRelationTest {
     }
 
     @Test
-    public void samePeerTest() {
-        SeqRelation relation = new SeqRelation("relation", new Pair<Pair<UMLClass, Integer>, Pair<UMLClass, Integer>>(createPair(classes.get(0), 0), createPair(classes.get(1), 0)));
-        Assert.assertFalse(relation.setSecond(createPair(classes.get(0) 0)));   // we probably dont want to allow self-reference
+    public void samePeerTest2() {
+        SeqRelation relation = new SeqRelation("relation", null, classes.get(0), 0, classes.get(1), 0);
+        Assert.assertFalse(relation.setSecond(classes.get(0), 0));   // we probably dont want to allow self-reference
     }
 
     @Test
     public void setMethodTest() {
-        classes.get(0).addAttribute(method);
-        SeqRelation relation = new SeqRelation("relation", new Pair<Pair<UMLClass, Integer>, Pair<UMLClass, Integer>>(createPair(classes.get(0), 0), createPair(classes.get(1), 0)));
+        SeqRelation relation = new SeqRelation("relation", null, classes.get(0), 0, classes.get(1), 0);
         Assert.assertTrue(relation.setMethod("method(10, \"string\")"));
         Assert.assertTrue(relation.getMethod().equals("method"));
-        Assert.assertTrue(relation.checkMethod());
+        Assert.assertTrue(relation.checkCorrect());
     }
 
     @Test
     public void setNoteTest() {
-        SeqRelation relation = new SeqRelation("relation", new Pair<Pair<UMLClass, Integer>, Pair<UMLClass, Integer>>(createPair(classes.get(0), 0), createPair(classes.get(1), 0)));
+        SeqRelation relation = new SeqRelation("relation", null, classes.get(0), 0, classes.get(1), 0);
         relation.setNote("note this");
         Assert.assertTrue(relation.getNote().equals("note this"));
     }
 
     @Test
     public void changeMethodParamsTest() {
-        classes.get(0).addAttribute(method);
-        SeqRelation relation = new SeqRelation("relation", new Pair<Pair<UMLClass, Integer>, Pair<UMLClass, Integer>>(createPair(classes.get(0), 0), createPair(classes.get(1), 0)));
+        SeqRelation relation = new SeqRelation("relation", null, classes.get(0), 0, classes.get(1), 0);
         relation.setMethod("method(10, \"string\")");
         method.setParameters(new String[] {"int", "string", "int"});
-        Assert.assertFalse(relation.checkMethod());
+        Assert.assertFalse(relation.checkCorrect());
     }
 
     @Test
     public void deleteMethodTest() {
-        classes.get(0).addAttribute(method);
-        SeqRelation relation = new SeqRelation("relation", new Pair<Pair<UMLClass, Integer>, Pair<UMLClass, Integer>>(createPair(classes.get(0), 0), createPair(classes.get(1), 0)));
+        SeqRelation relation = new SeqRelation("relation", null, classes.get(0), 0, classes.get(1), 0);
         relation.setMethod("method(10, \"string\")");
-        // TODO
+        classes.get(0).removeMethod(0);
+        Assert.assertFalse(relation.checkCorrect());
     }
 
     @Test
     public void wrongParamCountTest() {
         method.setParameters(new String[] {"int"});
-        SeqRelation relation = new SeqRelation("relation", new Pair<Pair<UMLClass, Integer>, Pair<UMLClass, Integer>>(createPair(classes.get(0), 0), createPair(classes.get(1), 0)));
+        SeqRelation relation = new SeqRelation("relation", null, classes.get(0), 0, classes.get(1), 0);
         Assert.assertFalse(relation.setMethod("method()"));
     }
 
     @Test
     public void oneParamTest() {
         method.setParameters(new String[] {"int"});
-        SeqRelation relation = new SeqRelation("relation", new Pair<Pair<UMLClass, Integer>, Pair<UMLClass, Integer>>(createPair(classes.get(0), 0), createPair(classes.get(1), 0)));
+        SeqRelation relation = new SeqRelation("relation", null, classes.get(0), 0, classes.get(1), 0);
         Assert.assertTrue(relation.setMethod("method(10)"));
     }
 
     @Test
     public void zeroParamTest() {
         method.setParameters(new String[] {});
-        SeqRelation relation = new SeqRelation("relation", new Pair<Pair<UMLClass, Integer>, Pair<UMLClass, Integer>>(createPair(classes.get(0), 0), createPair(classes.get(1), 0)));
-        Assert.assertFalse(relation.setMethod("method(10)"));   // there should be nothing between parenthesis
-        Assert.assertTrue(relation.setMethod("method()"));
+        SeqRelation relation = new SeqRelation("relation", null, classes.get(0), 0, classes.get(1), 0);
+        Assert.assertFalse(relation.setMethod("method(10)"));   // there should be nothing between parenthesis except white spaces
+        Assert.assertTrue(relation.setMethod("method(   )"));
     }
 
     @Test
     public void setNotMethodTest() {
-        classes.get(0).addAttribute(attribute);
-        SeqRelation relation = new SeqRelation("relation", new Pair<Pair<UMLClass, Integer>, Pair<UMLClass, Integer>>(createPair(classes.get(0), 0), createPair(classes.get(1), 0)));
+        SeqRelation relation = new SeqRelation("relation", null, classes.get(0), 0, classes.get(1), 0);
         Assert.assertFalse(relation.setMethod("attribute(nieco)"));
     }
 }
