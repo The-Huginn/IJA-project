0d913d1e02deaa57b4ded4411e314262da1b1529
The-Huginn <rastislavbudinsky@gmail.com>
2022-05-01 14:55:06 +0200

    Add GUI entities for class and sequence diagrams.

---
+++ src/main/java/com/ija/GUI/classDiagram/cUMLDiagram.java
@@ -0,0 +1,49 @@
+package com.ija.GUI.classDiagram;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+import com.ija.GUI.UMLElement;
+import com.ija.backend.diagram.Diagram;
+
+import javafx.scene.control.Label;
+
+public class cUMLDiagram extends UMLElement {
+    private Label name;
+
+    private Deque<UndoType> undo_stack = new ArrayDeque<>();
+
+    private enum UndoType {
+        others
+    }
+
+    public cUMLDiagram(Diagram diagram, Label name) {
+        super(diagram, ElementType.CLASS_DIAGRAM);
+
+        this.name = name;
+    }
+
+    @Override
+    public void select() {}
+
+    @Override
+    public void unselect() {}
+
+    @Override
+    public void addUndo() {
+        undo_stack.addFirst(UndoType.others);
+        name.setText(getElement().getName());
+    }
+
+    @Override
+    public void undo() {
+
+        if (undo_stack.isEmpty())
+            return;
+
+        undo_stack.pop();
+
+        super.undo();
+        name.setText(getElement().getName());
+    }
+}
---
+++ src/main/java/com/ija/GUI/seqDiagram/sUMLDiagram.java
@@ -0,0 +1,49 @@
+package com.ija.GUI.seqDiagram;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+import com.ija.GUI.UMLElement;
+import com.ija.backend.diagram.Diagram;
+
+import javafx.scene.control.Label;
+
+public class sUMLDiagram extends UMLElement {
+    private Label name;
+
+    private Deque<UndoType> undo_stack = new ArrayDeque<>();
+
+    private enum UndoType {
+        others
+    }
+
+    public sUMLDiagram(Diagram diagram, Label name) {
+        super(diagram, ElementType.SEQ_DIAGRAM);
+
+        this.name = name;
+    }
+
+    @Override
+    public void select() {}
+
+    @Override
+    public void unselect() {}
+
+    @Override
+    public void addUndo() {
+        undo_stack.addFirst(UndoType.others);
+        name.setText(getElement().getName());
+    }
+
+    @Override
+    public void undo() {
+
+        if (undo_stack.isEmpty())
+            return;
+
+        undo_stack.pop();
+
+        super.undo();
+        name.setText(getElement().getName());
+    }
+}
