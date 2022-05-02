0c7e15db3ae3f7c27dc3d3d7616711c5b88192b8
The-Huginn <rastislavbudinsky@gmail.com>
2022-05-02 15:58:21 +0200

    Implement class relation.

--- src/main/java/com/ija/GUI/classDiagram/cUMLRelation.java
+++ src/main/java/com/ija/GUI/classDiagram/cUMLRelation.java
@@ -1,36 +1,93 @@
 package com.ija.GUI.classDiagram;
 
+import com.ija.Application.App;
+import com.ija.GUI.GraphicInterface;
 import com.ija.GUI.UMLElement;
 import com.ija.backend.diagram.ClassRelation;
+import com.ija.backend.diagramObject.Element;
 
+import javafx.event.EventHandler;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 import javafx.scene.shape.Line;
 
-public class cUMLRelation extends UMLElement {
+public class cUMLRelation implements GraphicInterface {
+    private final ClassRelation element;
+    private final cUMLDiagram parent;
+
     private Line line;
-    private String[] colors = {" blue;", " cyan;", " orange;", " black;", " pink;"};
+    private Label name;
+    private static final String[] colors = {" blue;", " cyan;", " orange;", " black;", " pink;"};
 
-    public cUMLRelation(ClassRelation element, cUMLDiagram parent, int y, int x) {
-        super(element, parent, ElementType.CLASS_RELATION);
+    public cUMLRelation(ClassRelation element, Pane parentPane, cUMLDiagram parent, double y, double x) {
 
+        this.element = element;
+        this.parent = parent;
+        
         line = new Line();
         line.setStartX(x);
         line.setStartY(y);
+
+        name = new Label(element.getName());
+
+        line.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                if (event.getButton() == MouseButton.PRIMARY)
+                    App.setSelected(cUMLRelation.this);
+            }
+        });
+
+        name.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                if (event.getButton() == MouseButton.PRIMARY)
+                    App.setSelected(cUMLRelation.this);
+            }
+        });
+
+        parentPane.getChildren().add(line);
+        parentPane.getChildren().addAll(name);        
+        name.toBack();
+        line.toBack();
+
         updateContent();
     }
 
-    public void drawEnd(int y, int x) {
+    public void drawEnd(double y, double x) {
         line.setEndY(y);
         line.setEndX(x);
+        name.setLayoutY((line.getEndY() + line.getStartY()) / 2);
+        name.setLayoutX((line.getEndX() + line.getStartX()) / 2);
     }
 
-    public void drawStart(int y, int x) {
+    public void drawStart(double y, double x) {
         line.setStartY(y);
         line.setStartX(x);
+        name.setLayoutY((line.getEndY() + line.getStartY()) / 2);
+        name.setLayoutX((line.getEndX() + line.getStartX()) / 2);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other)
+            return true;
+
+        if (!(other instanceof cUMLRelation))
+            return false;
+
+        cUMLRelation otherThis = (cUMLRelation)other;
+
+        return otherThis.line == this.line &&
+                otherThis.getElement() == this.getElement();
     }
 
     @Override
     public void select() {
         line.setStyle("-fx-stroke: yellow");
+        name.setStyle("-fx-text-fill: yellow");
     }
 
     @Override
@@ -42,6 +99,7 @@ public class cUMLRelation extends UMLElement {
     public void updateContent() {
         // TODO
         line.setStyle("-fx-stroke:" + colors[((ClassRelation)getElement()).getType().ordinal()]);
+        name.setStyle("-fx-text-fill:" + colors[((ClassRelation)getElement()).getType().ordinal()]);
     }
 
     @Override
@@ -49,11 +107,33 @@ public class cUMLRelation extends UMLElement {
 
     @Override
     public void undo() {
-        super.undo();
+        element.undo();
         updateContent();
     }
     
     public void deleteMe() {
         
+    }
+
+    @Override
+    public UMLElement getUMLParent() {
+        return parent;
+    }
+
+    @Override
+    public void removeSelf(Pane fromPane) {
+        fromPane.getChildren().remove(line);
+        fromPane.getChildren().remove(name);
+        
+    }
+
+    @Override
+    public Element getElement() {
+        return element;
+    }
+
+    @Override
+    public ElementType getType() {
+        return ElementType.CLASS_RELATION;
     }
 }
